fa88785ccb666a74b07dc496b0667ca66d3995e1
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-08-07 15:43:23 -0300

    Remove the pseudocode

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-/*  REQUIREMENTS
-
-  - View an age in years, months, and days after submitting a valid date through the form
-  - Receive validation errors if:
-  - Any field is empty when the form is submitted
-  Show: "This field is required"
-  - The day number is not between 1-31
-  Show: "Must be a valid day"
-  - The month number is not between 1-12
-  Show: "Must be a valid month"
-  - The date is in the future
-  Show: "Must be in the past"
-  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
-  Show: "Must be a valid date" (Just one error message at the beginning)
-
-
-*/
-
 const dayInput = document.querySelector("input[name='day']");
 const monthInput = document.querySelector("input[name='month']");
 const yearInput = document.querySelector("input[name='year']");
